8a0b8f9d53ffa8dc1dd4ac4ae46ef979e48bc636
axmalm <axelmalmeling@outlook.com>
2024-09-16 12:33:55 +0200

    Update iregister.c

--- nina-lab1/a1p1/lib/iregister.c
+++ nina-lab1/a1p1/lib/iregister.c
@@ -54,6 +54,23 @@ void setBit(int i, iRegister *R)
 	}
 }
 
+void setAll(iRegister *r)
+{
+	// pre condition
+	if (r == NULL){
+		fprintf(stderr, " Error: A NULL pointer was given to setAll\n");
+		return;
+	}
+
+	r->content = ~0; 	
+
+	// post-condition
+	if((r->content != -1)){
+		fprintf(stderr, "Error: Failed to set all bits to 1\n");
+		return;	
+	}
+}
+
 
 int getBit(int i, iRegister *R)
 {
@@ -97,6 +114,44 @@ int getBit(int i, iRegister *R)
 	}
 }
 
+void assignNibble(int a, int b, iRegister *r){
+
+	// pre-conditions
+	if (r == NULL){
+		fprintf(stderr, "Error: A NULL pointer was given to assignNibble\n");
+		return;
+	}
+	if (a < 0 || a > 15 || b < 0 || b > 15){
+		fprintf(stderr, "Error: Invalid a or b\n");
+		return;
+	}
+
+	int content_before = r->content;
+
+	// clear bits 0-7
+	r->content &= ~255;  // ~255 = 1111 1111 1111 1111 1111 1111 0000 0000
+	// add a
+	r->content += a;
+	// add b
+	r->content += (b << 4);
+
+	// post-condition
+	// Check bits 0-3
+	if ((r->content & 15) != a) { // bitmask 0000 0000 0000 0000 0000 0000 0000 1111 
+		fprintf(stderr, "Error: Bits 0-3 does not match '%d'\n", a);
+		return;
+	}
+	// Check bits 4-7
+	if (((r->content >> 4) & 15) != b){
+		fprintf(stderr, "Error: Bits 4-7 does not match '%d'\n", b);
+		return;
+	}
+	// Check bits 8-31
+	if ((r->content & ~255) != (content_before & ~255)){
+		fprintf(stderr, "Error: Bits 8-31 has ben altered", b);
+		return;
+	}
+}
 
 int getNibble(int i, iRegister *R)
 {
@@ -133,6 +188,36 @@ int getNibble(int i, iRegister *R)
 	}
 }
 
+char *reg2str(iRegister r){
+
+	// pre-condition
+
+	char *bitArray = (char *)malloc(33 * sizeof(char)); 
+	int j = 0;
+
+	for (unsigned int i = 1U << 31; i > 0; i /= 2){ // shifting a mask from 1000 0000 0000 0000 0000 0000 0000 0000 -> 0*31 1
+		if (r.content & i){
+			bitArray[j] = '1';
+		}
+		else{
+			bitArray[j] = '0';
+		}
+		j++;
+	}
+
+	bitArray[32] = '\0'; // Null-terminate, printf purposes
+	
+	// post-conditions
+	// iRegister instance stays unaltered since it's passed by value, not by reference
+	if (j != 32) {
+        fprintf(stderr, "Error: Incorrect length of the bit string. Expected 32 but got %d\n", j);
+        free(bitArray);
+        return NULL;
+    }
+
+	return bitArray;
+
+}
 
 void shiftRight(int i, iRegister *R)
 {
@@ -166,7 +251,33 @@ void shiftRight(int i, iRegister *R)
 
 }
 
+void shiftLeft(int i, iRegister *r){
 
+	// pre-condition
+	if (r == NULL){
+		fprintf(stderr, "Error: A NULL pointer was given to shiftLeft\n");
+	}
+	if (!(i >= 0)){
+		fprintf(stderr, "Error: value of i invalid\n");
+	}
+
+	for (int j = 0; j < i; j++){
+		r->content <<= 1;
+	}
+
+	// post-conditions
+	// Check that i bits to the right are 0's
+	int mask = (1 << i) - 1;
+	if ((r->content & mask) != 0) {
+		fprintf(stderr, "Error: Bits to the right are not 0\n");
+		return;
+	}
+	// Check that the rest is unaltered
+	if ((r->content >> i) != r->content){
+		fprintf(stderr, "Error: Failed to shift bits to the left\n");
+		return;
+	}
+}
 
 void resetBit(int i, iRegister *r)
 {
